3c2078625f53220b7a573b4775c3bd3d7b84a768
kshitizbista <tsetiz.bista@gmail.com>
2019-09-22 12:00:02 +0200

    added project file

---
+++ src/main/java/rc/bootsecurity/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package rc.bootsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
+                .and()
+                .withUser("kshitiz")
+                .password(passwordEncoder().encode("kshitiz"))
+                .roles("USER")
+                .and()
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager"))
+                .roles("MANAGER").authorities("ACCESS_TEST1");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //.anyRequest().authenticated() // any request is available for authenticated user
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/*").authenticated()
+                .antMatchers("/admin/*").hasRole("ADMIN")
+                .antMatchers("/management/*").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
